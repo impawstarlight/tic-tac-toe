b97498939dfeb7c51f9b395d01bf9d217e84051e
Abdul Kaium <abdulkaium1024@gmail.com>
2025-08-18 13:15:07 +0600

    cleanup

--- app/components/GameBoard.tsx
+++ app/components/GameBoard.tsx
@@ -4,12 +4,12 @@ import SVGIcon from "./SVGIcon";
 const winningLines = [
   [0, 1, 2],
   [3, 4, 5],
-  [6, 7, 8],
+  [6, 7, 8], // horizontal
   [0, 3, 6],
   [1, 4, 7],
-  [2, 5, 8],
+  [2, 5, 8], // vertical
   [0, 4, 8],
-  [2, 4, 6],
+  [2, 4, 6], // diagonal
 ];
 
 export default function GameBoard({
@@ -21,7 +21,7 @@ export default function GameBoard({
   setWinner: (winner: string) => void;
   updateScore: (winner: "X" | "O") => void;
 }) {
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [cells, setCells] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(0);
   const [winnerCells, setWinnerCells] = useState(Array(3).fill(null));
 
@@ -43,7 +43,7 @@ export default function GameBoard({
 
   return (
     <div className="fill-foreground bg-foreground/10 grid w-full max-w-100 grid-cols-3 gap-2">
-      {board.map((cell, index) => (
+      {cells.map((cell, index) => (
         <div
           className="bg-background flex aspect-square items-center justify-center"
           key={index}
@@ -52,14 +52,13 @@ export default function GameBoard({
             key={index}
             onClick={() => {
               if (cell || turn >= 9 || winnerCells[1]) return; // ignore if cell is already filled or game is over
-              const newBoard = [...board];
-              newBoard[index] = turn % 2 === 0 ? "X" : "O";
-              checkWinner(newBoard);
-              setBoard(newBoard);
+              const newCells = [...cells];
+              newCells[index] = turn % 2 === 0 ? "X" : "O";
+              checkWinner(newCells);
+              setCells(newCells);
               setTurn(turn + 1);
               setCurrentPlayer(turn % 2 === 0 ? "O" : "X");
             }}
-            // className={`bg-foreground/10 focus:bg-foreground/20 hover:bg-foreground/15 active:bg-foreground/25 flex aspect-square items-center justify-center rounded-lg transition-colors focus:outline-none ${winnerCells.includes(index) && "fill-primary"}`}
             className={`active:bg-primary/15 flex size-[80%] items-center justify-center rounded-full transition-colors focus:outline-none ${winnerCells.includes(index) && "bg-primary fill-background"}`}
           >
             {cell == "X" ? (
