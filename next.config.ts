21adbe8987f55eb56da9dae03071bb29be986a71
Abdul Kaium <abdulkaium1024@gmail.com>
2025-09-08 08:25:32 +0600

    fix build typo

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // output: "export",
-  // basePath: "/tic-tac-toes",
+  // basePath: "/tic-tac-toe",
   // distDir: "/docs",
 };
 
