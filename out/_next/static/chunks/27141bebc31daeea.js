6a9851f932b9decfa1b8d77ffc32e9ee43ecaa63
Abdul Kaium <abdulkaium1024@gmail.com>
2025-09-08 07:30:35 +0600

    static build github pages

---
+++ out/_next/static/chunks/27141bebc31daeea.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/23671f9592b998e9.js",
+  "static/chunks/2f74e583e226d0b8.js",
+  "static/chunks/turbopack-1bfde08b7bf911c9.js"
+])
---
+++ out/_next/static/qUoNPEVujQG90uyrNqrvC/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/35aceea38cfa70af.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
